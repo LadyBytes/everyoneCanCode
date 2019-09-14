e5f06b6ffedbb1a1581c5819307dafde13270e86
Lisa Wagner <wagnerl@mskcc.org>
2019-09-14 17:27:57 -0400

    bash examples

---
+++ bash.sh
@@ -0,0 +1,19 @@
+cd path/to/myProject    // change directory
+
+cd ..                   // go up one level
+
+pwd                     // show current location
+
+mkdir project           // create new directory
+
+touch file.txt          // create new file
+
+open .                  // open current folder
+
+
+
+
+
+
+
+
